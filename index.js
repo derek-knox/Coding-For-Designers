dc37ba66f0fbbb146aa859b781a01098689eaaec
Derek Knox <derek-knox@users.noreply.github.com>
2018-01-21 11:41:37 -0800

    updated cover and readme and build num

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ Metalsmith(__dirname)
   .metadata({
     title: "Coding for Designers",
     description: "...",
-    version: '1.0.39'
+    version: '1.0.40'
   })
   .source('./book')
   .destination('./site')
