8e31c63efe80fea4e9515774733546df3d7da0d0
Derek Knox <derek-knox@users.noreply.github.com>
2018-01-27 11:53:07 -0800

    updated twitter card image and font size fix on home page for mobile

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ Metalsmith(__dirname)
   .metadata({
     title: "Coding for Designers",
     description: "...",
-    version: '1.0.65'
+    version: '1.0.66'
   })
   .source('./book')
   .destination('./site')
