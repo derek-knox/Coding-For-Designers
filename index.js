d4d21cd43606261bb89a66a05d9fe851f93ac419
Derek Knox <derek-knox@users.noreply.github.com>
2018-04-10 16:02:28 -0700

    updated build w/recent twitter description update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ Metalsmith(__dirname)
   .metadata({
     title: "Coding for Designers",
     description: "...",
-    version: '1.1.18'
+    version: '1.1.19'
   })
   .source('./book')
   .destination('./site')
