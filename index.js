17051f7ee82c4fcee5914ae889f254bbb72d88ff
Derek Knox <derek-knox@users.noreply.github.com>
2018-10-02 18:33:18 -0700

    updated placeholder graphic, updated version

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ Metalsmith(__dirname)
   .metadata({
     title: "Coding for Designers",
     description: "...",
-    version: '1.1.35'
+    version: '1.1.38'
   })
   .source('./book')
   .destination('./site')
